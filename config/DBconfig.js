b492c028c1ccb73a8f9f373a3262c06c0e76e535
alan-um <urquizamanzanelli.aj@gmail.com>
2024-11-14 23:57:06 -0300

    Update DBconfig.js

--- config/DBconfig.js
+++ config/DBconfig.js
@@ -3,19 +3,19 @@ const mysql = require("mysql2/promise");
 const crearConexion = async () => {
     const connection = await mysql.createConnection({
         //DB Local
-        host: 'localhost',
+        /* host: 'localhost',
         user: 'root',
         password: '',
         database: 'atenmedica1',
         port: 3306,
-        multipleStatements: true
+        multipleStatements: true */
 
         //DB CC
-        /* host: 'bdk3g5vaeedryozkkdd7-mysql.services.clever-cloud.com',
+        host: 'bdk3g5vaeedryozkkdd7-mysql.services.clever-cloud.com',
         user: 'uy7r6mevogm0fiys',
         password: 'Xxe3MqxR1x4FGeb7TODP',
         database: 'bdk3g5vaeedryozkkdd7',
-        port: 3306 */
+        port: 3306
     });
     return connection;
 }
